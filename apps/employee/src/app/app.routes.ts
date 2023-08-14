92adf0c66a8dba4d533d259c96db9f6f17f10145
Sunil Mekala <c-smekala@pa.gov>
2023-08-14 16:04:31 -0400

    Added another remote ToDo application to the host Employee application

--- apps/employee/src/app/app.routes.ts
+++ apps/employee/src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
+    path: 'todo',
+    loadChildren: () =>
+      loadRemoteModule('todo', './Module').then((m) => m.RemoteEntryModule),
+  },
+  {
     path: 'login',
     loadChildren: () =>
       loadRemoteModule('login', './Module').then((m) => m.RemoteEntryModule),
